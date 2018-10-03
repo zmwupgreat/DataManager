5315e3a015b3decc4ff5c1eb3df8ac6b5c6df2e8
zmwupgreat <844833466@qq.com>
2018-10-03 20:05:11 +0800

    add inputwidget insert database

--- inputwidget.cpp
+++ inputwidget.cpp
@@ -1,4 +1,4 @@
-#include "inputwidget.h"
+﻿#include "inputwidget.h"
 #include "ui_inputwidget.h"
 
 InputWidget::InputWidget(QWidget *parent) :
@@ -6,9 +6,95 @@ InputWidget::InputWidget(QWidget *parent) :
     ui(new Ui::InputWidget)
 {
     ui->setupUi(this);
+    ui->comboBoxgender->addItem("男");
+    ui->comboBoxgender->addItem("女");
+    ui->dateEditBirth->setCalendarPopup(true);
+    QDate date = QDate::currentDate();
+    ui->dateEditBirth->setDisplayFormat("yyyy/MM/dd");
+    ui->dateEditBirth->setDate(date);
+    connect(ui->toolButtoncancel,SIGNAL(clicked(bool)),SLOT(close()));
+    connect(ui->toolButtonCheck,SIGNAL(clicked(bool)),SLOT(InsertBtn()));
+    connect(ui->FiletoolButton,SIGNAL(clicked(bool)),SLOT(SelectImage()));
+    this->setWindowFlags(Qt::FramelessWindowHint);
+    this->setWindowModality(Qt::ApplicationModal);
 }
 
 InputWidget::~InputWidget()
 {
     delete ui;
+}
+/*
+ * 获取图片
+ */
+void InputWidget::SelectImage()
+{
+    ImagePath = QFileDialog::getOpenFileName(NULL,tr("选择数据库文件"),".","*.jpg *.bmp *.png *.jpeg");
+        if(ImagePath.isEmpty())
+            return;
+        else
+        {
+            QImage photo;
+            photo.load(ImagePath);
+            photo = photo.scaled(231,131,Qt::KeepAspectRatio);
+            ui->labelPhoto->setPixmap(QPixmap::fromImage(photo));
+        }
+}
+
+/*
+ * 确定按钮槽，插入数据
+ */
+void InputWidget::InsertBtn()
+{
+    InfoData InsertInfo;
+    if(ui->lineEditID->text().isEmpty()
+            ||ui->lineEditName->text().isEmpty()
+            ||ui->lineEditNation->text().isEmpty())
+    {
+           QMessageBox::warning(NULL,"提示","请确定输入完整ID，姓名，民族","确定");
+           return;
+    }
+    InsertInfo.ID = ui->lineEditID->text();
+    InsertInfo.Name = ui->lineEditName->text();
+    QDateTime time = QDateTime::currentDateTime();
+    InsertInfo.Time = time.toString("yyyy/MM/dd hh:mm:ss");
+    InsertInfo.Birthtime = ui->dateEditBirth->text();
+    InsertInfo.Gender = ui->comboBoxgender->currentText();
+    InsertInfo.Nation = ui->lineEditNation->text();
+    if(ui->lineEditAddress->text().isEmpty())
+        InsertInfo.Address = " ";
+    else
+        InsertInfo.Address = ui->lineEditAddress->text();
+    if(ui->lineEditoffice->text().isEmpty())
+        InsertInfo.Office = " ";
+    else
+        InsertInfo.Office = ui->lineEditoffice->text();
+    QString ImageFile = ImagePath;
+    QString newfilename = "../../data/Image/"+InsertInfo.ID+".jpg";
+    QFile::copy(ImageFile,newfilename);
+    InsertInfo.Image = newfilename;
+    if(!DBcontrol->InsertData(InsertInfo))
+    {
+        emit sendRet(false);
+        this->close();
+        return;
+    }
+    else
+    {
+        emit sendRet(true);
+        this->close();
+        return;
+    }
+}
+
+/*
+ * 清理输入框
+ */
+void InputWidget::ClearEdit()
+{
+    ui->lineEditID->clear();
+    ui->lineEditName->clear();
+    ui->lineEditNation->clear();
+    ui->lineEditoffice->clear();
+    ui->lineEditAddress->clear();
+    ui->labelPhoto->clear();
 }
--- inputwidget.h
+++ inputwidget.h
@@ -1,7 +1,18 @@
-#ifndef INPUTWIDGET_H
+﻿#ifndef INPUTWIDGET_H
 #define INPUTWIDGET_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QDateTimeEdit>
+#include <QComboBox>
+#include <QMessageBox>
+#include <QImage>
+#include <QFile>
+#include <QFileDialog>
+#include <QDateTime>
+#include "struct_define.h"
+#include "databasecontrol.h"
 
 namespace Ui {
 class InputWidget;
@@ -14,9 +25,17 @@ class InputWidget : public QWidget
 public:
     explicit InputWidget(QWidget *parent = 0);
     ~InputWidget();
+    void ClearEdit();
 
 private:
     Ui::InputWidget *ui;
+    DataBaseControl *DBcontrol = DataBaseControl::GetDatabase();//数据库对象
+    QString ImagePath;
+public slots:
+    void InsertBtn();
+    void SelectImage();
+signals:
+    void sendRet(bool);
 };
 
 #endif // INPUTWIDGET_H
