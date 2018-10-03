987cca6b0590840a9a6c6989791c3fec24ccb0d2
zmwupgreat <844833466@qq.com>
2018-10-03 20:06:08 +0800

    add treewidget,tablewidget

--- widget.cpp
+++ widget.cpp
@@ -6,12 +6,32 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    InitObject();
     CheckDir();
+    this->setWindowFlags(Qt::FramelessWindowHint);
+    this->setWindowModality(Qt::ApplicationModal);
 }
+/*
+ * 初始化模块
+ */
+void Widget::InitObject()
+{
+    Inputwidget = new InputWidget;
+    TableModel = new QSqlQueryModel;
+    TableViewInit("");
+    TreeWidgetInit();
 
+    connect(ui->toolButtonInput,SIGNAL(clicked(bool)),SLOT(btnInput()));
+    connect(ui->toolButtonexit,SIGNAL(clicked(bool)),SLOT(btnExit()));
+    connect(ui->toolButtonDelete,SIGNAL(clicked(bool)),SLOT(btnDelete()));
+    connect(Inputwidget,SIGNAL(sendRet(bool)),SLOT(handleInputResultMSG(bool)));
+    connect(ui->treeWidget,SIGNAL(itemClicked(QTreeWidgetItem*,int)),SLOT(TreeitemClick(QTreeWidgetItem*,int)));
+
+}
 Widget::~Widget()
 {
     delete ui;
+    delete Inputwidget;
 }
 /*
  * 检查数据目录是否完整
@@ -20,18 +40,226 @@ void Widget::CheckDir()
 {
     QDir dataDir;
     QDir databaseDir;
+    QDir ImageDir;
 
-    QString dataPath = "data/";
-    QString dbPath = "data/database/";
-
+    QString dataPath = "../../data/";
+    QString dbPath = "../../data/database/";
+    QString imagepath = "../../data/Image/";
 
     if(!dataDir.exists(dataPath))
         dataDir.mkpath(dataPath);
     if(!databaseDir.exists(dbPath))
         databaseDir.mkpath(dbPath);
+    if(!ImageDir.exists(imagepath))
+        ImageDir.mkpath(imagepath);
+    DBcontrol->GetDatabase();
 }
+/*
+ * 输入信息
+ */
+void Widget::btnInput()
+{
+    Inputwidget->ClearEdit();
+    Inputwidget->show();
+}
+/*
+ * 处理输入窗口返回信息
+ */
+void Widget::handleInputResultMSG(bool ret)
+{
+    if(ret)
+        QMessageBox::warning(NULL,"提示","信息写入成功","确定");
+    else
+        QMessageBox::warning(NULL,"提示","信息写入失败","确定");
+    TableViewInit("");
+    TreewidgetIteminit();
+}
+/*
+ * 退出程序
+ */
+void Widget::btnExit()
+{
+    this->close();
+    qApp->exit(0);
+}
+/*
+ * 删除槽
+ */
+void Widget::btnDelete()
+{
 
+}
+/*
+ * 初始化表结构
+ */
+void Widget::TableViewInit(QString condition)
+{
+    //设置tableview的属性
+    ui->tableView->setShowGrid(true);
+    ui->tableView->horizontalHeader()->setStretchLastSection(true);
+    ui->tableView->horizontalHeader()->setDefaultAlignment(Qt::AlignLeft);
+    ui->tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tableView->verticalHeader()->setVisible(false);
+    TableModel->clear();
+    DBcontrol->SelectallData(TableModel,condition,0);
+    ui->tableView->setModel(TableModel);
+    //设置表头
+    TableModel->setHeaderData(0,Qt::Horizontal,"证件ID");
+    TableModel->setHeaderData(1,Qt::Horizontal,"姓名");
+    TableModel->setHeaderData(2,Qt::Horizontal,"录入时间");
+    TableModel->setHeaderData(3,Qt::Horizontal,"性别");
+    TableModel->setHeaderData(4,Qt::Horizontal,"民族");
+    TableModel->setHeaderData(5,Qt::Horizontal,"出生年月");
+    TableModel->setHeaderData(6,Qt::Horizontal,"地址");
+    TableModel->setHeaderData(7,Qt::Horizontal,"签发单位");
+    TableModel->setHeaderData(8,Qt::Horizontal,"照片地址");
+    ui->tableView->show();
+}
+/*
+ * 初始化树结构
+ */
+void Widget::TreeWidgetInit()
+{
+    ui->treeWidget->setHeaderLabel("日期");
+    TreewidgetIteminit();
+}
+/*
+ * 删除所有树节点
+ */
+void Widget::deleteTreeItem()
+{
+   for(int i=0;i<ui->treeWidget->topLevelItemCount();i++)
+   {
+       for(int j=0;j<ui->treeWidget->topLevelItem(i)->childCount();j++)
+       {
+           for(int k=0;k<ui->treeWidget->topLevelItem(i)->child(j)->childCount();k++)
+           {
+               if(ui->treeWidget->topLevelItem(i)->child(j)->child(k))
+               {
+                   delete ui->treeWidget->topLevelItem(i)->child(j)->child(k);
+               }
+           }
+           if(ui->treeWidget->topLevelItem(i)->child(j))
+           {
+               delete ui->treeWidget->topLevelItem(i)->child(j);
+           }
+       }
+       if(ui->treeWidget->topLevelItem(i))
+       {
+           delete ui->treeWidget->topLevelItem(i);
+       }
+   }
+}
+/*
+ * 初始化树节点
+ */
+void Widget::TreewidgetIteminit()
+{
+    deleteTreeItem();
+    QVector<QString> Timevector = DBcontrol->getTimedata();
+    if(Timevector.size() == 0)
+        return;
+        for(int row=0;row<Timevector.size();row++)
+        {//在数据库对象中的结构体数组中查询获取时间
+            QString Time_date=Timevector.at(row);
+            QStringList Time_temp_List=Time_date.split(" ");//时间分离
+            QStringList Time_list=Time_temp_List.at(0).split('/');//日期分离
+            //分离日期生成树节点
+            QString year=Time_list.at(0)+" 年";
+            QString month=Time_list.at(1)+" 月";
+            QString day=Time_list.at(2)+" 日";
+            bool day_flag=false;
+            bool month_flag=false;
+            bool year_flag=false;
+            for(int i=0;i<ui->treeWidget->topLevelItemCount();i++)
+            {
+                if(ui->treeWidget->topLevelItem(i)->text(0)==year)
+                {
+                    for(int j=0;j<ui->treeWidget->topLevelItem(i)->childCount();j++)
+                    {
+                        if(ui->treeWidget->topLevelItem(i)->child(j)->text(0)==month)
+                        {
+                            for(int m=0;m<ui->treeWidget->topLevelItem(i)->child(j)->childCount();m++)
+                            {
 
+                                if(ui->treeWidget->topLevelItem(i)->child(j)->child(m)->text(0)==day)
+                                {
+                                    day_flag=true;
+                                    break;
+                                }
+                            }
+                            if(!day_flag)
+                            {
+                                QTreeWidgetItem *day_item=new QTreeWidgetItem;
+                                day_item->setText(0,day);
+                                ui->treeWidget->topLevelItem(i)->child(j)->addChild(day_item);
+                            }
+                            month_flag=true;
+                            break;
+                        }
+                    }
+                    if(!month_flag)
+                    {
+                        QTreeWidgetItem *month_item=new QTreeWidgetItem;
+                        QTreeWidgetItem *day_item=new QTreeWidgetItem;
+                        month_item->setText(0,month);
+                        day_item->setText(0,day);
+                        month_item->addChild(day_item);
+                        ui->treeWidget->topLevelItem(i)->addChild(month_item);
+                    }
+                    year_flag=true;
+                    break;
+                }
+            }
+            if(!year_flag)
+            {
+                QTreeWidgetItem *year_item=new QTreeWidgetItem;
+                QTreeWidgetItem *month_item=new QTreeWidgetItem;
+                QTreeWidgetItem *day_item=new QTreeWidgetItem;
+                year_item->setText(0,year);
+                month_item->setText(0,month);
+                day_item->setText(0,day);
+                month_item->addChild(day_item);
+                year_item->addChild(month_item);
+                int yearcount=ui->treeWidget->topLevelItemCount();
+                ui->treeWidget->insertTopLevelItem(yearcount,year_item);
+            }
+        }
+}
+/*
+ * 树节点相应槽
+ */
+void Widget::TreeitemClick(QTreeWidgetItem *item, int column)
+{
+    QString condition;
+    if(item->parent()==NULL)
+        {
+            QString temp=item->text(column).mid(0,4);
+            condition=QString("and Time LIKE '%1%'").arg(temp);
+            TableViewInit(condition);
+            Treeitem_select=true;
+        }
+        else if(item->parent()->parent())
+        {
+            QString temp_day=item->text(column).mid(0,2);
+            QString temp_month=item->parent()->text(column).mid(0,2);
+            QString temp_year=item->parent()->parent()->text(column).mid(0,4);
+            QString temp=temp_year+"/"+temp_month+"/"+temp_day;
+            condition=QString("and Time LIKE '%1%'").arg(temp);
+            TableViewInit(condition);
+            Treeitem_select=true;
+        }
+        else if(item->parent())
+        {
+            QString temp_year=item->parent()->text(column).mid(0,4);
+            QString temp_month=item->text(column).mid(0,2);
+            QString temp=temp_year+"/"+temp_month;
+            condition=QString("and Time LIKE '%1%'").arg(temp);
+            TableViewInit(condition);
+            Treeitem_select=true;
+        }
+}
 /*
  * 鼠标事件重写
  */
@@ -51,6 +279,5 @@ void Widget::mouseReleaseEvent(QMouseEvent *e)
 {
     int dx=e->globalX()-last.x();
     int dy=e->globalY()-last.y();
-
     move(x()+dx,y()+dy);
 }
--- widget.h
+++ widget.h
@@ -6,11 +6,15 @@
 #include <QtSql/QSqlDatabase>
 #include <QtSql/QSqlQuery>
 #include <QtSql/QSqlQueryModel>
+#include <QMessageBox>
 #include <QTableView>
+#include <QMouseEvent>
+#include <QVector>
 #include <QStandardItemModel>
 #include <QTreeWidgetItem>
 #include "databasecontrol.h"
 #include "struct_define.h"
+#include "inputwidget.h"
 
 namespace Ui {
 class Widget;
@@ -23,21 +27,32 @@ class Widget : public QWidget
 public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
-    CheckDir();
-    void TableViewInit();
+    void InitObject();//初始化模块
+    void CheckDir();//检查文件路径
+    void TableViewInit(QString condition);//初始化表结构
+    void TreeWidgetInit();//初始化树结构
+    void deleteTreeItem();//删除所有树节点
+    void TreewidgetIteminit();//根据数据库时间生成树节点
+
 public slots:
     void btnInput();//添加信息槽
+    void handleInputResultMSG(bool ret);//接收信息输入结果
+    void btnExit(); //退出槽
+    void btnDelete();//删除槽
+    void TreeitemClick(QTreeWidgetItem *item, int column);//点击树节点相应槽
+
 private:
     QPoint last;//智能指针 用于界面移动鼠标事件重写
     Ui::Widget *ui;
     QSqlQueryModel *TableModel;//数据表显示模型
-    DataBaseControl *DBControl = DataBaseControl::GetDatabase();//获取数据库类指针
+    InputWidget *Inputwidget;//输入窗口
+    DataBaseControl *DBcontrol = DataBaseControl::GetDatabase();//获取数据库指针
+    bool Treeitem_select;
 
 protected://重写鼠标事件，使窗口可移动
         void mousePressEvent(QMouseEvent *e);
         void mouseMoveEvent(QMouseEvent *e);
         void mouseReleaseEvent(QMouseEvent *e);
-        void paintEvent(QPaintEvent *e);
 };
 
 #endif // WIDGET_H
